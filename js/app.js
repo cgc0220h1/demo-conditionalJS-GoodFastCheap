583e044546c020a849e257e1b217dbc5d355fb6b
vanduc2514 <vanduc2514@gmail.com>
2020-02-23 02:51:29 +0700

    First commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-di
+function check(){
+    let ischeckedGood = document.getElementById("checkGood").checked;
+    let ischeckedFast = document.getElementById("checkFast").checked;
+    let ischekedCheap = document.getElementById("checkCheap").checked;
+    if (ischeckedGood && ischeckedFast && ischekedCheap) {
+        let currentState = Math.floor(Math.random() * 2) + 1 ;
+        alert(currentState);
+        switch (currentState) {
+            case 1:
+                document.getElementById("checkGood").checked = false;
+                break;
+            case 2:
+                document.getElementById("checkFast").checked = false;
+                break;
+            case 3:
+                document.getElementById("checkCheap").checked = false;
+                break;
+        }
+    }
+}
