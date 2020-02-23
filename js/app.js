25ea8278aab6012d5f94035beecc0dbc2d85bd2e
vanduc2514 <vanduc2514@gmail.com>
2020-02-24 01:08:10 +0700

    Refactor và thêm tính năng thay đổi ngẫu nhiên trạng thái công tắc.

--- js/app.js
+++ js/app.js
@@ -4,20 +4,31 @@ let ischekedCheap;
 let stateGood;
 let stateFast;
 let stateCheap;
-let stateCurrent;
+let statePrevious;
 
-function check() {
-    console.log("state-start: " + stateCurrent);
-    switch (stateCurrent) {
-        case "ab0":
+function flipSwitch() {
+    console.log("state-start: " + statePrevious);
+    if (statePrevious === "ab0") {
+        let rand = Math.random();
+        if (rand < 0.5) {
             document.getElementById('checkFast').checked = false;
-            break;
-        case "a0c":
+        } else {
             document.getElementById('checkGood').checked = false;
-            break;
-        case "0bc":
+        }
+    } else if (statePrevious === "a0c") {
+        let rand = Math.random();
+        if (rand < 0.5) {
+            document.getElementById('checkGood').checked = false;
+        } else {
             document.getElementById('checkCheap').checked = false;
-            break;
+        }
+    } else if (statePrevious === "0bc") {
+        let rand = Math.random();
+        if (rand < 0.5) {
+            document.getElementById('checkCheap').checked = false;
+        } else {
+            document.getElementById('checkFast').checked = false;
+        }
     }
     ischeckedGood = document.getElementById("checkGood").checked;
     ischeckedFast = document.getElementById("checkFast").checked;
@@ -25,6 +36,6 @@ function check() {
     stateGood = (ischeckedGood) ? "a" : 0;
     stateFast = (ischeckedFast) ? "b" : 0;
     stateCheap = (ischekedCheap) ? "c" : 0;
-    stateCurrent = stateGood + stateFast + stateCheap;
-    console.log("state-end: " + stateCurrent);
+    statePrevious = stateGood + stateFast + stateCheap;
+    console.log("state-end: " + statePrevious);
 }
