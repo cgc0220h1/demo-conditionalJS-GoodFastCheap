51d7291a9153d15f91d502ba42e296f1f0b434b1
vanduc2514 <vanduc2514@gmail.com>
2020-02-27 10:09:41 +0700

    Sửa lại thuật toán

--- js/app.js
+++ js/app.js
@@ -1,41 +1,22 @@
-let ischeckedGood;
-let ischeckedFast;
-let ischekedCheap;
-let stateGood;
-let stateFast;
-let stateCheap;
-let statePrevious;
-
-function flipSwitch() {
-    console.log("state-start: " + statePrevious);
-    if (statePrevious === "ab0") {
-        let rand = Math.random();
-        if (rand < 0.5) {
-            document.getElementById('checkFast').checked = false;
-        } else {
-            document.getElementById('checkGood').checked = false;
-        }
-    } else if (statePrevious === "a0c") {
-        let rand = Math.random();
-        if (rand < 0.5) {
-            document.getElementById('checkGood').checked = false;
-        } else {
-            document.getElementById('checkCheap').checked = false;
-        }
-    } else if (statePrevious === "0bc") {
-        let rand = Math.random();
-        if (rand < 0.5) {
-            document.getElementById('checkCheap').checked = false;
-        } else {
-            document.getElementById('checkFast').checked = false;
-        }
+function flipSwitch(id) {
+    let isCheckedGood = document.getElementById('checkGood');
+    let isCheckedFast = document.getElementById('checkFast');
+    let isCheckedCheap = document.getElementById('checkCheap');
+    switch (id) {
+        case "checkGood":
+            if (isCheckedFast.checked) {
+                isCheckedCheap.checked = false;
+            }
+            break;
+        case "checkFast":
+            if (isCheckedCheap.checked) {
+                isCheckedGood.checked = false;
+                }
+            break;
+        case "checkCheap":
+            if (isCheckedGood.checked) {
+                isCheckedFast.checked = false;
+            }
+            break;
     }
-    ischeckedGood = document.getElementById("checkGood").checked;
-    ischeckedFast = document.getElementById("checkFast").checked;
-    ischekedCheap = document.getElementById("checkCheap").checked;
-    stateGood = (ischeckedGood) ? "a" : 0;
-    stateFast = (ischeckedFast) ? "b" : 0;
-    stateCheap = (ischekedCheap) ? "c" : 0;
-    statePrevious = stateGood + stateFast + stateCheap;
-    console.log("state-end: " + statePrevious);
 }
